6b642f8c92817515986bf7f6064de58ae321b61e
Dov Salomon <dsalomon@nyu.edu>
2020-05-03 17:21:56 -0400

    bilinear maps with simple driver

--- bilinear.py
+++ bilinear.py
@@ -3,69 +3,61 @@ from pypbc import *
 import hashlib
 import secrets
 import os
+import math
 
 class Key:
     def __init__(self, params, priv, pubg, pubh):
         self.params = params
-        self.priv = pubg
+        self.pairing = Pairing(params)
+        self.priv = priv
         self.pubg = pubg
         self.pubh = pubh
 
-    # def store(self, d):
-    #     with open(os.path.join(d, 'a.priv'), 'w') as f:
-    #         f.write(str(self.priv))
-    #     with open(os.path.join(d, 'a.pubg'), 'w') as f:
-    #         f.write(str(self.pubg))
-    #     with open(os.path.join(d, 'a.pubh'), 'w') as f:
-    #         f.write(str(self.pubh))
-    #     with open(os.path.join(d, 'a.params'), 'w') as f:
-    #         f.write(str(self.params))
-
-    # @classmethod
-    # def load(cls, d):
-    #     with open(os.path.join(d, 'a.params'), 'r') as f:
-    #         pstr = f.read(str(self.params))
-    #     with open(os.path.join(d, 'a.priv'), 'r') as f:
-    #         privstr = (str(self.priv))
-    #     with open(os.path.join(d, 'a.pubg'), 'r') as f:
-    #         gstr = f.read(str(self.pubg))
-    #     with open(os.path.join(d, 'a.pubh'), 'r') as f:
-    #         hstr = f.read(str(self.pubh))
+        rstr = [l for l in str(params).split('\n') if l.startswith('r')]
+        assert(len(rstr) == 1)
+        rstr = rstr[0]
+        self.p = int(rstr.split(' ')[1])
+        self.logp = math.ceil(math.log(self.p, 2))
 
 class BilinearMap:
-    def __init__(self, keywords):
-        self.keywords = keywords
+    def __init__(self, kw=[], key=None, s=160):
+        self.keywords = kw
+        self.key = key or self.genkey(s=s)
 
-    def genkey(self, s):
-        # p = secrets.randbits(s/2)
-        # q = secrets.randbits(s/2)
-        # n = p*q
-        params = Parameters(qbits=512, rbits=160)
+    def genkey(self, s=160):
+        params = Parameters(qbits=512, rbits=s)
         pairing = Pairing(params)
         priv = Element.random(pairing, Zr)
         pubg = Element.random(pairing, G1)
         pubh = pubg ** priv
-        self.key = Key(params, priv, pubg, pubh)
+        return Key(params, priv, pubg, pubh)
+
+    def H1(self, W):
+        W_hash = hashlib.sha512(W.encode('utf-8')).digest()
+        H1_W = Element.from_hash(self.key.pairing, G1, W_hash)
+        return H1_W
+
+    def H2(self, G2):
+        s = str(G2)
+        s_hash = hashlib.sha512(s.encode('utf-8')).digest()
+        H2_hash = s_hash[:math.ceil(self.key.logp/8)]
+        return H2_hash
 
     def peks(self, W):
+        if W not in self.keywords:
+            raise Exception("invalid keyword")
         r = Element.random(self.key.pairing, Zr)
         hr = self.key.pubh ** r
         gr = self.key.pubg ** r
 
-        W_hash = hashlib.sha512(W.encode('utf-8')).digest()
-        H1_W = Element.from_hash(self.key.pairing, G1, W_hash)
-
-        t = self.pairing.apply(H1_W, hr)
-        # t_hash = hashlib.sha512(
-        # H2_t = Element.from_hash(self.key.pairing, G1,
-        # return (gr, E
+        H1_W = self.H1(W)
+        t = self.key.pairing.apply(H1_W, hr)
+        H2_t = self.H2(t)
+        return (gr, H2_t)
 
     def trapdoor(self, W):
-        W_hash = hashlib.sha512(W.encode('utf-8')).digest()
-        H1_W = Element.from_hash(self.key.pairing, G1, W_hash)
-        return H1_W ** self.key.priv
+        return self.H1(W) ** self.key.priv
 
-    def test(self, ciphertext, trapdoor):
-        A, B = ciphertext
-
-
+    def test(self, S, Tw):
+        A, B = S
+        return self.H2(self.key.pairing.apply(Tw, A)) == B
---
+++ peks.py
@@ -0,0 +1,17 @@
+
+from sys import argv
+import bilinear
+
+s = int(argv[1])
+kw = argv[2]
+
+peks = bilinear.BilinearMap(kw=argv[3:], s=s)
+ciphers = [peks.peks(W) for W in argv[3:]]
+Tw = peks.trapdoor(kw)
+
+for i, S in enumerate(ciphers):
+    if peks.test(S, Tw):
+        print("kw is:", argv[i+3])
+        exit(1)
+
+print("kw not found")
