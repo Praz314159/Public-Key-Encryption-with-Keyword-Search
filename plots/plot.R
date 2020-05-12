efc186a150b17f696da62dcd2242f3dc611b9c1e
Dov Salomon <dsalomon@nyu.edu>
2020-05-11 23:12:57 -0400

    parallelize the testing code

--- plots/plot.R
+++ plots/plot.R
@@ -19,7 +19,7 @@ q <- ggplot(td, aes(x=N,y=GEN,color=as.factor(SP),group=as.factor(SP))) +
   theme_bw(base_size=12) +
   theme(plot.title = element_text(hjust=0.5))
 
-ggsave(plot=q, "trapdoor_gen.png")
+# ggsave(plot=q, "trapdoor_gen.png")
 
 q <- ggplot(bm, aes(x=N,y=PEKS,color=as.factor(SP),group=as.factor(SP))) +
   geom_point() +
@@ -33,4 +33,32 @@ q <- ggplot(bm, aes(x=N,y=PEKS,color=as.factor(SP),group=as.factor(SP))) +
   theme_bw(base_size=12) +
   theme(plot.title = element_text(hjust=0.5))
 
-ggsave(plot=q, "bm_peks.png")
+# ggsave(plot=q, "bm_peks.png")
+
+# Plot d(PEKS)/dN against the security parameter (bilinear)
+
+spx = unique(bm$SP)
+slopes = c()
+for (sp in spx) {
+  bmsp <- bm[bm$SP == sp, ]
+  model <- lm(bmsp$PEKS ~ bmsp$N)
+  slopes <- c(slopes, model$coefficients[2])
+}
+dPEKSdN <- data.frame("SP" = spx, "rate" = slopes)
+
+# Plot KeyGen against security parameter (bilinear scheme)
+q <- ggplot(dPEKSdN, aes(x=SP, y=rate)) +
+  geom_point() +
+  geom_line() +
+  labs(
+    title = "Rate of Bilinear Map PEKS",
+    x = "Order of P (bits)",
+    y = "Rate"
+  ) +
+  theme_bw(base_size=12) +
+  theme(plot.title = element_text(hjust=0.5))
+
+# Plot PEKS against security parameter (group by N) (trapdoor permutations)
+
+# (optional) d(PEKS)/d(SP) (group by N) (trapdoor permutation)
+
--- plots/run_test.sh
+++ plots/run_test.sh
@@ -9,16 +9,14 @@ echo "MODE,SP,N,GEN,PEKS,TRAPDOOR,TEST" > "$out"
 
 # mode = bm
 for sp in 80 160 240 320 400 512 600; do
-	for n in $(python -c "for i in range(14): print(2**i)"); do
-		seq "$n" > "$tmp" &
-		python ../peks.py -sp "$sp" -m bm -t nope -kf "$tmp" --time >> "$out"
+	for n in $(python -c "for i in range(8): print(2**i)"); do
+		parallel -j8 "mkfifo /tmp/{} 2>/dev/null; seq $n > /tmp/{} & python ../peks.py -sp $sp -m bm -t nope -kf /tmp/{} --time >> $out" ::: $(seq 50)
 	done
 done
 
 # mode = td
 for sp in 1024 2048 4096; do
-	for n in $(python -c "for i in range(12): print(2**i)"); do
-		seq "$n" > "$tmp" &
-		python ../peks.py -sp "$sp" -m td -t nope -kf "$tmp" --time >> "$out"
+	for n in $(python -c "for i in range(8): print(2**i)"); do
+		parallel -j8 "mkfifo /tmp/{} 2>/dev/null; seq $n > /tmp/{} & python ../peks.py -sp $sp -m td -t nope -kf /tmp/{} --time >> $out" ::: $(seq 50)
 	done
 done
